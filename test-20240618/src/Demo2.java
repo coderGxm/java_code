c8889be8678632db25f54a85e9c2095c38d1dbf4
coderGxm <18684188183@139.com>
2024-06-18 14:30:44 +0800

    test-20240618

--- test-20240618/src/Demo2.java
+++ test-20240618/src/Demo2.java
@@ -1,5 +1,12 @@
 public class Demo2 {
     public static void main(String[] args) {
         System.out.println("hello!");
+        System.out.println("hello!");
+        System.out.println("hello!");
+        System.out.println("hello!");
+        System.out.println("hello!");
+        System.out.println("hello!");
+        System.out.println("hello!");
+        System.out.println("hello!");
     }
 }
